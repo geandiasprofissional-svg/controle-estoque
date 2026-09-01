a8e7ab3675d89963b7f0eafc960f326d566c4411
Gean Dias Ferreira <gean.dias.profissional@gmail.com>
2026-09-01 13:17:17 -0300

    fix: prepara backend para deploy

--- backend/app.js
+++ backend/app.js
@@ -3,74 +3,30 @@ const cors = require("cors");
 
 require("./database/createTables");
 
-const produtoRoutes =
-    require("./routes/ProdutoRoutes");
-
-const movimentacaoRoutes =
-    require("./routes/MovimentacaoRoutes");
-
-const fornecedorRoutes =
-    require("./routes/FornecedorRoutes");
-
-const produtoFornecedorRoutes =
-    require("./routes/ProdutoFornecedorRoutes");
-
-const dashboardRoutes =
-    require("./routes/DashboardRoutes");
-
+const produtoRoutes = require("./routes/ProdutoRoutes");
+const movimentacaoRoutes = require("./routes/MovimentacaoRoutes");
+const fornecedorRoutes = require("./routes/FornecedorRoutes");
+const produtoFornecedorRoutes = require("./routes/ProdutoFornecedorRoutes");
+const dashboardRoutes = require("./routes/DashboardRoutes");
 
 const app = express();
-
 
 app.use(cors());
 
 app.use(express.json());
 
-
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-
     res.send("Servidor funcionando!");
-
 });
 
+app.use("/produtos", produtoRoutes);
+app.use("/movimentacoes", movimentacaoRoutes);
+app.use("/fornecedores", fornecedorRoutes);
+app.use("/produto-fornecedor", produtoFornecedorRoutes);
+app.use("/dashboard", dashboardRoutes);
 
-app.use(
-    "/produtos",
-    produtoRoutes
-);
-
-
-app.use(
-    "/movimentacoes",
-    movimentacaoRoutes
-);
-
-
-app.use(
-    "/fornecedores",
-    fornecedorRoutes
-);
-
-
-app.use(
-    "/produto-fornecedor",
-    produtoFornecedorRoutes
-);
-
-
-app.use(
-    "/dashboard",
-    dashboardRoutes
-);
-
-
-app.listen(PORT, () => {
-
-    console.log(
-        `Servidor rodando na porta ${PORT}`
-    );
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
